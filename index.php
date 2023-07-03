cb105b544f8902e9bce36f3ce97e96fda3ec9360
CarloSalaris <carloa.salaris@gmail.com>
2023-07-03 18:00:55 +0200

    Products in an array + Dog/Cat category

--- index.php
+++ index.php
@@ -6,9 +6,9 @@ class Product {
     private $imgSrc;
     private $price;
     private $type;
-    private $category;
+    private Category $category;
 
-    public function __construct($title, $imgSrc, $price, $category, $type) {
+    public function __construct($title, $imgSrc, $price, Category $category, $type) {
 
         $this -> setTitle($title);
         $this -> setImgSrc($imgSrc);
@@ -18,14 +18,15 @@ class Product {
 
     }
 
-    public function getProductInfo() {
+    public function getProductInfo($number) {
 
-        return "PRODOTTO 1: <br>"
+        return "PRODOTTO $number: <br>"
         . "<br> Titolo: " . $this -> getTitle()
         . "<br> Immagine: " . $this -> getImgSrc()
         . "<br> Prezzo: " . $this -> getPrice() . " Euro"
-        . "<br> Categoria: " . $this -> getCategory()
-        . "<br> Type: " . $this -> getType();
+        . "<br> Categoria: " . $this -> getCategory() -> getCategName()
+        . "<br> Icona: " . $this -> getCategory() -> getIcon()
+        . "<br> Tipo: " . $this -> getType();
 
     }
 
@@ -66,6 +67,60 @@ class Product {
 
 }
 
-$product1 = new Product("spazzola", "./img/CatDogBrush.jpg", 4.50, "Cani", "prodotto");
+class Category {
 
-echo $product1 -> getProductInfo();
+    private $name;
+    private $icon;
+
+    public function __construct($name, $icon) {
+        
+        $this -> setCategName($name);
+        $this -> setIcon($icon);
+    }
+
+    public function getCategName() {
+
+        return $this -> name;
+    }
+
+    public function setCategName($name) {
+        
+        if ($name != "Cani" AND $name != "Gatti") {
+       
+            throw new Exception("Category needs to be 'Cani' or 'Gatti'.");
+
+        }
+
+        $this -> name = $name;
+    }
+
+    public function getIcon() {
+        
+        return $this -> icon;
+    }
+    
+    public function setIcon($icon) {
+
+        $this -> icon = $icon;
+
+    }
+
+}
+
+//categories
+$dogProds = new Category("Cani", "dog Icon");
+$catProds = new Category("Gatti", "cat Icon");
+
+
+//products
+$products = [
+    new Product("spazzola", "./img/CatDogBrush.jpg", 4.50, $dogProds, "prodotti"),
+    new Product("croccantini", "", 20.35, $catProds, "cibo"),
+    new Product("osso", "", 3.90, $dogProds, "giochi")
+];
+
+foreach ($products as $key => $value) {
+
+    echo $value -> getProductInfo($key + 1) . "<br><br>--------------------<br><br>";
+
+};
