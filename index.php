2ab4322d1eb149c9eb1ec8c36ca0e41112d999ad
CarloSalaris <carloa.salaris@gmail.com>
2023-07-04 11:14:59 +0200

    Prima parte correzione in classe Food extends Product

--- index.php
+++ index.php
@@ -1,72 +1,5 @@
 <?php
 
-class Product {
-
-    private $title;
-    private $imgSrc;
-    private $price;
-    private $type;
-    private Category $category;
-
-    public function __construct($title, $imgSrc, $price, Category $category, $type) {
-
-        $this -> setTitle($title);
-        $this -> setImgSrc($imgSrc);
-        $this -> setPrice($price);
-        $this -> setCategory($category);
-        $this -> setType($type);
-
-    }
-
-    public function getProductInfo($number) {
-
-        return "PRODOTTO $number: <br>"
-        . "<br> Titolo: " . $this -> getTitle()
-        . "<br> Immagine: " . $this -> getImgSrc()
-        . "<br> Prezzo: " . $this -> getPrice() . " Euro"
-        . "<br> Categoria: " . $this -> getCategory() -> getCategName()
-        . "<br> Icona: " . $this -> getCategory() -> getIcon()
-        . "<br> Tipo: " . $this -> getType();
-
-    }
-
-    public function getTitle() {
-        return $this -> title;
-    }
-    public function setTitle($title) {
-        $this -> title = $title;
-    }
-
-    public function getImgSrc() {
-        return $this -> imgSrc;
-    }
-    public function setImgSrc($imgSrc) {
-        $this -> imgSrc = $imgSrc;
-    }
-
-    public function getPrice() {
-        return $this -> price;
-    }
-    public function setPrice($price) {
-        $this -> price = $price;
-    }
-
-    public function getCategory() {
-        return $this -> category;
-    }
-    public function setCategory($category) {
-        $this -> category = $category;
-    }
-
-    public function getType() {
-        return $this -> type;
-    }
-    public function setType($type) {
-        $this -> type = $type;
-    }
-
-}
-
 class Category {
 
     private $name;
@@ -107,6 +40,100 @@ class Category {
 
 }
 
+class Product {
+
+    private $name;
+    private $imgSrc;
+    private $price;
+    private $type;
+    
+    private Category $category;
+
+    public function __construct($name, $imgSrc, $price, Category $category, $type) {
+
+        $this -> setName($name);
+        $this -> setImgSrc($imgSrc);
+        $this -> setPrice($price);
+        $this -> setType($type);
+
+        $this -> setCategory($category);
+
+    }
+
+    public function getProductInfo($number) {
+
+        return "PRODOTTO $number: <br>"
+        . "<br> Titolo: " . $this -> getTitle()
+        . "<br> Immagine: " . $this -> getImgSrc()
+        . "<br> Prezzo: " . $this -> getPrice() . " Euro"
+        . "<br> Categoria: " . $this -> getCategory() -> getCategName()
+        . "<br> Icona: " . $this -> getCategory() -> getIcon()
+        . "<br> Tipo: " . $this -> getType();
+
+    }
+
+    public function getTitle() {
+        return $this -> title;
+    }
+    public function setName($name) {
+        $this -> title = $name;
+    }
+
+    public function getImgSrc() {
+        return $this -> imgSrc;
+    }
+    public function setImgSrc($imgSrc) {
+        $this -> imgSrc = $imgSrc;
+    }
+
+    public function getPrice() {
+        return $this -> price;
+    }
+    public function setPrice($price) {
+        $this -> price = $price;
+    }
+
+    public function getCategory() {
+        return $this -> category;
+    }
+    public function setCategory($category) {
+        $this -> category = $category;
+    }
+
+    public function getType() {
+        return $this -> type;
+    }
+    public function setType($type) {
+        $this -> type = $type;
+    }
+
+}
+
+class Food extends Product {
+
+    private $expireDate;
+
+    public function __construct(
+            $name, $imgSrc, $price, $category, $type,
+            $expireDate
+        ) {
+
+        parent :: __construct($name, $imgSrc, $price, $category, $type);
+        
+        $this -> setExpireDate($expireDate);
+    }
+
+    public function getExpireDate() {
+
+        return $this -> expireDate;
+    }
+
+    public function setExpireDate($expireDate) {
+        $this -> expireDate = $expireDate;
+    }
+
+}
+
 //categories
 $dogProds = new Category("Cani", "dog Icon");
 $catProds = new Category("Gatti", "cat Icon");
@@ -122,5 +149,18 @@ $products = [
 foreach ($products as $key => $value) {
 
     echo $value -> getProductInfo($key + 1) . "<br><br>--------------------<br><br>";
+
+};
+
+//Estensioni products
+$food = [
+    new Food("biscotti", "", 4.50, $dogProds, "cibo", "20/01/2024"),
+    new Food("croccantini", "", 20.35, $catProds, "cibo", "20/01/2024"),
+    new Food("osso", "", 3.90, $dogProds, "cibo", "20/01/2024")
+];
+
+foreach ($food as $key => $value) {
+
+    echo $value -> getProductInfo($key + 1) . "<br> Scadenza: " . $value -> getExpireDate() . "<br><br>--------------------<br><br>";
 
 };
